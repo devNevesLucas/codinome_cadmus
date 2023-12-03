ffca314a329d4dcdcf68f0c4454985c23b3f2428
Marcelo-122 <marcelinhojr12@hotmail.com>
2023-12-03 02:15:08 -0300

    Adição dos sprites
    -11 Sprites adicionados no case do getProjetil e no sprites txt.

--- codinome_cadmus/Mecanicas/getSpriteProjetil/getSpriteProjetil.c
+++ codinome_cadmus/Mecanicas/getSpriteProjetil/getSpriteProjetil.c
@@ -19,6 +19,41 @@ const char* getSpriteProjetil( int codSprite ) {
         case 4:
             return "Auxiliar/sprites/projeteis/blocoVerde.png";
 
+            //Ines
+        case 5:
+            return "Auxiliar/sprites/projeteis/ines/caveira.png";
+
+        case 6:
+            return "Auxiliar/sprites/projeteis/ines/ossogrande.png";
+
+        case 7:
+            return "Auxiliar/sprites/projeteis/ines/ossosimples.png";
+
+            //Adamastor
+        case 8:
+            return "Auxiliar/sprites/projeteis/adamastor/onda.png";
+
+        case 9:
+            return "Auxiliar/sprites/projeteis/adamastor/punhoD.png";
+
+        case 10:
+            return "Auxiliar/sprites/projeteis/adamastor/punhoE.png";
+
+        case 11:
+            return "Auxiliar/sprites/projeteis/adamastor/raio.png";
+            //Dionisio
+        case 12:
+            return "Auxiliar/sprites/projeteis/dionisio/barco.png";
+
+        case 13:
+            return "Auxiliar/sprites/projeteis/dionisio/bom.png";
+
+        case 14:
+            return "Auxiliar/sprites/projeteis/dionisio/mal.png";
+
+        case 15:
+            return "Auxiliar/sprites/projeteis/dionisio/poison.png";
+
         default:
             return "Auxiliar/sprites/projeteis/bloco.png";
     }
